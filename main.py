7d65fa658026a08643ce6aa00d53cdf89a1d2d8f
Joshua Langelaar <93121879+DeCooleCoder@users.noreply.github.com>
2022-06-02 08:53:19 +0000

    Woordlijst toegevoegd en Poppetje toegevoegd

--- main.py
+++ main.py
@@ -1,3 +1,99 @@
+import random
+
+woordlist = ["informatica", "informatiekunde", "spelletje", "spelletje", "aardigheidje", "sholier", "fotografie", "waardebepaling", "specialiteit", "verzekering", "universiteit", "heesterperk"]
+
+def getthaword():
+  word = random.choice(woordlist)
+  return word.upper() 
+#alle letters worden in uppercase gezet voor beter leesbaareid
+
+def play(word):
+  word_compleet = "_" * length(word)
+  guessed = False
+  guessed_letters = []
+  guessed_words = []
+  tries = 6
+
+
+
+
+#Poppetje
+def display_hangman(tries):
+    stages = [  # OOoooo je bent bijna dood o-o
+                """
+                   --------
+                   |      |
+                   |      O
+                   |     \\|/
+                   |      |
+                   |     / \\
+                   -
+                """,
+                # oke je hebt nog een kans
+                """
+                   --------
+                   |      |
+                   |      O
+                   |     \\|/
+                   |      |
+                   |     / 
+                   -
+                """,
+                # zowat bijna halverwege
+                """
+                   --------
+                   |      |
+                   |      O
+                   |     \\|/
+                   |      |
+                   |      
+                   -
+                """,
+                # je hebt nu al 3 keer gefaald
+                """
+                   --------
+                   |      |
+                   |      O
+                   |     \\|
+                   |      |
+                   |     
+                   -
+                """,
+                # 2x gefaald
+                """
+                   --------
+                   |      |
+                   |      O
+                   |      |
+                   |      |
+                   |     
+                   -
+                """,
+                # 1 keer gefaald
+                """
+                   --------
+                   |      |
+                   |      O
+                   |    
+                   |      
+                   |     
+                   -
+                """,
+                # Humble Beginnings
+                """
+                   --------
+                   |      |
+                   |      
+                   |    
+                   |      
+                   |     
+                   -
+                """
+    ]
+    return stages[tries]
+
+
+
 #Game engine
 def game():
   print("yay! you won!")
